0f8a49de201b9368993254f2871e28cbde6e37a5
Den <denniskov82@gmail.com>
2023-02-26 17:15:57 +0200

    Text

--- cw-02/math.js
+++ cw-02/math.js
@@ -5,3 +5,20 @@ console.log(firstNumber+secondNumber);
 console.log(firstNumber-secondNumber);
 console.log(firstNumber*secondNumber);
 console.log(firstNumber/secondNumber);
+
+console.log(firstNumber%secondNumber);
+console.log(11%2);
+console.log(4%2);
+console.log(6%3);
+
+let firstUserValue = prompt('Введіть перше число');
+let secondUserValue = prompt('Введіть друге число');
+
+//let result = +firstUserValue + +secondUserValue;
+let result = parseInt(firstUserValue) + parseFloat(secondUserValue);
+//console.log(parseInt(firstUserValue));
+//console.log(parseFloat(secondUserValue));
+
+console.log(Number(firstUserValue));
+console.log(secondUserValue);
+
