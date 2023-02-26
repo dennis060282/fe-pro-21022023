40645782fc76b0c40fdcb1b557a8cdfdf6137978
Den <denniskov82@gmail.com>
2023-02-26 15:02:21 +0200

    Text

--- cw-01/myscript.js
+++ cw-01/myscript.js
@@ -1,2 +1,25 @@
 //alert('Hello users!');
+let variableName= '#333';
+var oldVariable = 'Arial';
+const USERAPIKEY = 'secretPassword';
 
+// string
+let userName = "Max";
+let USERNAME = 'Tom';
+let user_name = 'Bob';
+let UserName = 'Lara';
+
+let husbandName = 'Tom';
+let wifeName = 'Mary';
+
+let stringNumbers = '12343545'; 
+console.log("This value from stringNumbers", stringNumbers);
+//alert(wifeName);
+//Number
+
+let firstNumber = 1;
+let secondNumber = 1.3;
+console.log('Max value = ', Number.MAX_VALUE);
+console.log('Min value = ', Number.MIN_VALUE);
+
+console.log(firstNumber);
