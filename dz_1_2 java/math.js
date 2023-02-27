368106a3bb0e509b059e5d0602c4a3c4963d5131
Den <denniskov82@gmail.com>
2023-02-27 19:56:11 +0200

    Text

---
+++ dz_1_2 java/math.js
@@ -0,0 +1,4 @@
+let valueLenght = prompt('Введіть довжину, см');
+let valueLWidth = prompt('Введіть ширину, см');
+let valueArea = Number(valueLenght) * Number(valueLWidth);
+console.log("Площа прямокутника:", valueArea);
