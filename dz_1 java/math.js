be8a4e499c2b5d058b02245394c9a431da05860d
Den <denniskov82@gmail.com>
2023-02-27 19:41:14 +0200

    Text

---
+++ dz_1 java/math.js
@@ -0,0 +1,5 @@
+let userPrice = prompt('Введіть ціну за продукт, грн');
+let priceFor10 = Number(userPrice) * 0.97;
+console.log("Ваша ціна за придбання 10 одиниць товару:",priceFor10);
+let priceFor20 = Number(userPrice) * 0.95;
+console.log("Ваша ціна за придбання 20 одиниць товару:",priceFor20);
