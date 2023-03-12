816426921740f13de9d834c8d81621111f803980
Den <denniskov82@gmail.com>
2023-03-12 20:23:24 +0200

    Text

---
+++ funcion/001_function.js
@@ -0,0 +1,17 @@
+function myFirstFunction () {
+    document.write("<p>Hello my first");
+    document.write("<p>how do you do");
+    document.write("<p>good buy");
+}
+
+function mySecondFunction() {
+    document.write("<p>Hello my second");
+}
+
+myFirstFunction();
+
+document.write("<hr/>");
+
+mySecondFunction();
+
+document.write("<hr/>");
